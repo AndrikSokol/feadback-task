273c70657870bc7702818f4d0114271964150847
AndrikSokol <AndrikSokol31@gmail.com>
2023-10-29 00:02:02 +0300

    feat: added validate for inputs

--- src/js/modules/validationInputs.js
+++ src/js/modules/validationInputs.js
@@ -3,6 +3,8 @@
 document
   .querySelector(".form__button")
   .addEventListener("click", function (event) {
+    event.preventDefault();
+
     const nameInputElement = document.querySelector(".form__name");
     const emailInputElement = document.querySelector(".form__email");
     const phoneInputElement = document.querySelector(".form__phone");
@@ -15,17 +17,34 @@ document
       messageTextareaElement,
     ];
 
-    event.preventDefault();
+    function isValidEmail(email){
+      const regex= /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+      return regex.test(String(email).toLowerCase())
+     }
 
+    let isValidateInputs = true;
     elementsForValidate.forEach((element) => {
+      const formControl = element.parentElement;
+      const formError = formControl.querySelector(".form__error")
       if (element.value == "") {
-        element.style.border = "1px solid red";
-        element.innerHTML = "<p>This is field is required<p>";
-        element.appendChild(warningSpan);
-        setTimeout(
-          () => (element.style.border = "1px solid transparent"),
-          5000
-        );
+        isValidateInputs = false;
+        formControl.classList.add("form-control--error")
+        formControl.classList.remove("form-control--success")
+        formError.innerText="This field is required"
+      } else if (element === emailInputElement && !isValidEmail(element.value)) {
+        isValidateInputs = false;
+        formControl.classList.add("form-control--error");
+        formControl.classList.remove("form-control--success");
+        formError.innerText = "Please enter a valid email address";
+      } else {
+        formError.innerText = "";
+        formControl.classList.remove("form-control--error");
+        formControl.classList.add("form-control--success");
       }
-    });
+    })
+
+    if(isValidateInputs)
+    {
+            
+    }
   });
