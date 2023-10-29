7725c7ee7112ca63f313df1c498a9099110c2d64
AndrikSokol <AndrikSokol31@gmail.com>
2023-10-30 02:37:50 +0300

    fix: styles for modal

--- src/js/modules/modal.js
+++ src/js/modules/modal.js
@@ -3,17 +3,18 @@ const modal = document.querySelector(".modal");
 const closeModalBtn = document.querySelector(".button-modal--close");
 
 openModalBtn.addEventListener("click", () => {
-  modal.style.display = "flex";
+  modal.classList.add("modal--active");
   document.body.style.overflow = "hidden";
 });
 
 closeModalBtn.addEventListener("click", () => {
-  modal.style.display = "none";
+  modal.classList.remove("modal--active");
   document.body.style.overflow = "auto";
 });
 
 modal.addEventListener("click", (event) => {
   if (event.target == modal) {
-    modal.style.display = "none";
+    modal.classList.remove("modal--active");
+    document.body.style.overflow = "auto";
   }
 });
